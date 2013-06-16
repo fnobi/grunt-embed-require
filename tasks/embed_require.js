d23664d0eac1aa46de65434efb7faefa36c1aa4c
Fujisawa Shin <fujisawa-shin@in.kayac.com>
2013-06-16 17:07:52 +0900

    ModとModCollectionを分割。

--- tasks/embed_require.js
+++ tasks/embed_require.js
@@ -4,24 +4,27 @@ module.exports = function (grunt) {
 
         path = require('path'),
         _    = require('underscore'),
-        Mod  = require('../lib/Mod');
+        ModCollection = require('../lib/ModCollection');
 
     grunt.file.defaultEncoding = 'utf8';
 
     grunt.registerMultiTask(taskName, taskDescription, function () {
         var target = this.target,
             config = grunt.config(taskName)[target],
+
+            // config parameters
             srcPattern = config.srcPattern || '**/*.js',
             dest = config.dest || 'js',
             src = config.src || 'src',
+
             mapping = grunt.file.expandMapping(srcPattern, dest, { cwd: src }),
-            mods = {};
+            modCollection = new ModCollection();
 
         _.each(mapping, function (map) {
-            mods[path.resolve(map.src[0])] = new Mod(map, mods);
+            modCollection.add(path.resolve(map.src[0]), map);
         });;
 
-        _.each(mods, function (mod) {
+        _.each(modCollection.modules, function (mod) {
             console.log('[write] %s', mod.destPath);
             grunt.file.write(mod.destPath, mod.embeded());
         });;
